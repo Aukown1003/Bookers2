1dd45a948fe1e99c22e9f4fbd84debfe05d54aee
YsukeKuwano <115121908+Aukown1003@users.noreply.github.com>
2022-10-27 09:00:42 +0000

    [Add]headerレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root to: 'home#top'
   get 'home/about' => 'home#about', as: 'about'
   devise_for :users
-  resources :books
-  resources :users
+  resources :books, only:[:index, :show, :create, :edit, :update, :destroy]
+  resources :users, only:[:index, :show, :edit, :update]
   # name:bookers2_test
   # email:test@bookers2
   # pass:
