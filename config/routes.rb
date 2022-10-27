60bad70071ce38b28f6bb019df96fcc52cb9dd63
YsukeKuwano <115121908+Aukown1003@users.noreply.github.com>
2022-10-27 03:27:38 +0000

    [Add]controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root to: 'home#top'
   get 'home/about' => 'home#about', as: 'about'
   devise_for :users
+  resources :books
+  resources :users
   # name:bookers2_test
   # email:test@bookers2
-  # pass:testpass1
+  # pass:
 end
