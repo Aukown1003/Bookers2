fbf5482d17fde61f25cc09a4531b5174e3defd99
YsukeKuwano <115121908+Aukown1003@users.noreply.github.com>
2022-10-28 09:23:01 +0000

    [Add]投稿部分のテンプレート化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,25 @@
 class UsersController < ApplicationController
   def index
-    
+    @users = User.all
   end
-  
+
   def show
-    
+    @user = User.find(params[:id])
+    # @books = @user.books
   end
-  
+
   def edit
+    @user = User.find(params[:id])
   end
-  
+
   def update
-    
+    user_id = params[:id].to_i
+    login_user.id = current_user.id
+    if user_id != current_user.id
+      redirect_to user_path(current_user.id)
+    end
+    @user = User.find(params[:id])
+    @user.update
+    redirect_to user_path(@user.id)
   end
 end
